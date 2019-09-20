ef094393716850283a26b1e7005fd8d87bd1bc7e
Justin Miller <jayman1294@gmail.com>
2019-09-20 22:46:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,25 +1,9 @@
 def nyc_pigeon_organizer(data)
- data.reduce() do |memo, (key, value)|
-    [memo][:Name] 
-    
-  end
-   data
-end
 
-pigeon_list = {}
 
-pigeon_data.each do |property, property_hash|
-  property_hash.each do |values, birds_array|
-    birds_array.each do |name|
-      if pigeon_list[name]
-        if pigeon_list[name][property]
-          pigeon_list[name][property] << values
-        else
-          pigeon_list[name][property] = values
-        end
-      else
-        pigeon_list[name] = {property => [values]}
-      end
-    end
-  end
+
+
+
+
+
 end
